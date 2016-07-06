7b49d70c80cb08363d968c98c871569b5e3987a1
Danilo de Carvalho Souza <danilotremere@gmail.com>
2016-07-06 19:21:00 -0300

    Ajustado view de post#index e adicionado destroy aos comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,14 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :authenticate_user!, only: [:destroy]
 
   def create
     @post.comments.create! comments_params
     redirect_to @post
+  end
+
+  def destroy
+    @post.comments.destroy
   end
 
   private
